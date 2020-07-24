043b03dfbcdb60d5c59422f65eefb11f76411062
Benosmane Yassine <54205753+Fizioh@users.noreply.github.com>
2020-07-24 10:27:51 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,4 +3,18 @@
 const Circ = new mojs.Shape({
     isShowStart: true,
     radius: 100,
-})
+    //Animation
+    fill: {'#FC2079':'#fff'},
+    scale: {1:0},
+    opacity: {1:0},
+    left: '50%',
+    duration: 1500,
+    delay: 300,
+    easing: 'clubic.out',
+    isShowEnd: false,
+});
+
+document.addEventListener('click', function(e) {
+    Circ,
+    replay();
+});
